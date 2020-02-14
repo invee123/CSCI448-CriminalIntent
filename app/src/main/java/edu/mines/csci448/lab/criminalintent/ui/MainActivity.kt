f7c63c79a8d79ef783dbf43d11edb2ca85c65f2a
invee123 <g-money@csm-wl-dhcp-198-52.mines.edu>
2020-02-14 14:32:12 -0700

    First commit

---
+++ app/src/main/java/edu/mines/csci448/lab/criminalintent/ui/MainActivity.kt
@@ -0,0 +1,44 @@
+package edu.mines.csci448.lab.criminalintent.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import edu.mines.csci448.lab.criminalintent.R
+
+class MainActivity : AppCompatActivity() {
+
+    private val logTag = "448.MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(logTag, "onCreate() called")
+        setContentView(R.layout.activity_main)
+
+        // TODO 2 create fragment transaction
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(logTag, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(logTag, "onResume() called")
+    }
+
+    override fun onPause() {
+        Log.d(logTag, "onPause() called")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(logTag, "onStop() called")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(logTag, "onDestroy() called")
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/edu/mines/csci448/lab/criminalintent/ui/detail/CrimeDetailFragment.kt
@@ -0,0 +1,10 @@
+package edu.mines.csci448.lab.criminalintent.ui.detail
+
+import androidx.fragment.app.Fragment
+
+class CrimeDetailFragment : Fragment() {
+
+    private val logTag = "448.CrimeDetailFrag"
+
+    // TODO 3 override all Fragment Lifecycle methods and log their occurrance
+}
